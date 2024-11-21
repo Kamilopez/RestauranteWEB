e20b4ad54101441436e017eda9a569b761acc21d
jhonjairoqhm <jhonjairo1234qh@gmail.com>
2024-11-20 20:35:10 -0500

    Index platos

---
+++ Pages/Platos/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestauranteWEB.Data;
+using RestauranteWEB.Models;
+
+namespace RestauranteWEB.Pages.Platos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RestaurantContext _context;
+
+        public IndexModel(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Plato> Platos { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Ingredientes != null)
+            {
+                Platos = await _context.Platos.ToListAsync();
+            }
+        }
+    }
+}
