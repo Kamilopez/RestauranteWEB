0c2e671b565ded4d309ede757e3a9caad92a46a1
jhonjairoqhm <jhonjairo1234qh@gmail.com>
2024-11-20 20:41:43 -0500

    Create platos

---
+++ Pages/Platos/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestauranteWEB.Data;
+using RestauranteWEB.Models;
+
+namespace RestauranteWEB.Pages.Platos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RestaurantContext _context;
+
+        public CreateModel(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Plato Platos { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Platos == null || Platos == null)
+            {
+                return Page();
+            }
+
+            _context.Platos.Add(Platos);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
