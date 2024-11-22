784cbf2db3009d2cddc491f2eacccbcc7620098a
jhonjairoqhm <jhonjairo1234qh@gmail.com>
2024-11-21 23:00:54 -0500

    Indice de Pedidos

--- Pages/Pedidos/Index.cshtml.cs
+++ Pages/Pedidos/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestauranteWEB.Data;
+using RestauranteWEB.Models;
 
 namespace RestauranteWEB.Pages.Pedidos
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly RestaurantContext _context;
+
+        public IndexModel(RestaurantContext context)
         {
+            _context = context;
+        }
+
+        public IList<Pedido> Pedidos { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Pedidos != null)
+            {
+                Pedidos = await _context.Pedidos.ToListAsync();
+            }
         }
     }
 }
