dd36148961907c090141f9445ebfd4af6383876a
Juan Quiñonez <juandavid9812@gmail.com>
2024-11-22 19:28:31 -0500

    Actualizacion Create Proveedores

--- Pages/Proveedores/Create.cshtml.cs
+++ Pages/Proveedores/Create.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestauranteWEB.Data;
 
 namespace RestauranteWEB.Pages.Proveedores
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly RestaurantContext _context;
+
+        public CreateModel(RestaurantContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Plato Platos { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Platos == null || Platos == null)
+            {
+                return Page();
+            }
+
+            _context.Platos.Add(Platos);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
