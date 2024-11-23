1ed2d2c0d6a1407cda82b482a9bfd1c09c965c19
Juan Quiñonez <juandavid9812@gmail.com>
2024-11-22 19:26:30 -0500

    Creacion Razor Pages Create proveedores

---
+++ Pages/Proveedores/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestauranteWEB.Pages.Proveedores
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
